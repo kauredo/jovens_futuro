3594d22a3d21b3317d9a190e76e5f7b83f3218be
kauredo <vaskafig@gmail.com>
2021-02-18 17:19:19 +0000

    test again

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,38 +9,11 @@ const webpack = require('webpack');
 //======================= CONFIGURE DEFAULT LOADERS ==========================//
 const cssOpts = { modules: { localIdentName: '[name]_[local]_[hash:hex:5]' } };
 
-for (const name of ['moduleCss', 'moduleSass']) {
-	const loader = environment.loaders
-		.get(name)
-		.use.find(e => e.loader === 'css-loader');
-	loader.options = merge(loader.options, cssOpts);
-}
-
 for (const name of ['sass', 'moduleSass']) {
 	// resolve-url-loader must be used before sass-loader
 	environment.loaders.get(name).use.splice(-1, 0, {
 		loader: 'resolve-url-loader',
 	});
 }
-
-//================================ PLUGINS ===================================//
-
-// HACK: Make sure that z-index transformation is not applied. See:
-// https://github.com/rails/webpacker/pull/1673
-environment.plugins.append(
-	'OptimizeCSSAssets',
-	new OptimizeCSSAssetsPlugin({
-		cssProcessorOptions: {
-			safe: true,
-		},
-	})
-);
-
-environment.plugins.append(
-	'Provide',
-	new webpack.ProvidePlugin({
-		slick: ['slick-carousel', 'default'],
-	})
-);
 
 module.exports = environment;
