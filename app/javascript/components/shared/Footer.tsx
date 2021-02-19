70abcda26dc635da3b8dd2ebd4d84d866424fa0e
kauredo <vaskafig@gmail.com>
2021-02-19 21:32:56 +0000

    v1

--- app/javascript/components/shared/Footer.tsx
+++ app/javascript/components/shared/Footer.tsx
@@ -6,6 +6,7 @@ export default function Footer() {
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.container}>
+				<div className={styles.middle}>Criado por Jovens &o Futuro 2021</div>
 				<div className={styles.left}>
 					<a
 						href='mailto: jovenseofuturo2021@gmail.com'
@@ -14,7 +15,15 @@ export default function Footer() {
 						jovenseofuturo2021@gmail.com
 					</a>
 				</div>
-				<div className={styles.right}>Criado por Jovens &o Futuro 2021</div>
+				<div className={styles.right}>
+					Econtre-nos:{' '}
+					<a href='#' className={styles.sendEmail}>
+						<i className='fab fa-twitter'></i>
+					</a>
+					<a href='#' className={styles.sendEmail}>
+						<i className='fab fa-instagram'></i>
+					</a>
+				</div>
 			</div>
 		</footer>
 	);
