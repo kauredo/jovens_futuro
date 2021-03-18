465ec598b280e4efa4db91131e9d39a5f683ddd2
kauredo <vaskafig@gmail.com>
2021-03-18 23:34:00 +0000

    fix email 2.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,10 +68,10 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  host = 'jovens-futuro.herokuapp.com'
+  host = 'https://jovens-futuro.herokuapp.com/'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default charset: 'utf-8'
 
