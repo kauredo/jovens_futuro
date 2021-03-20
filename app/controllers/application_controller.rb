20edfc26861499141830c9cf2365daad90558ce1
kauredo <vaskafig@gmail.com>
2021-03-20 19:59:31 +0000

    add meta links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  def default_url_options
+    { host: ENV['DOMAIN'] || 'localhost:3000' }
+  end
+
   def after_sign_in_path_for(resource)
     backoffice_path
   end
