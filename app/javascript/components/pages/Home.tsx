8359030e1bd395ccf9af36220a6d1a540945abce
kauredo <vaskafig@gmail.com>
2021-11-17 19:38:12 +0000

    Experiment add color to sections in Home page

--- app/javascript/components/pages/Home.tsx
+++ app/javascript/components/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Artigo from '../artigos/Artigo';
 import { Article } from '../artigos/NovoArtigo';
 import ImageSlider from '../images/ImageSlider';
@@ -49,14 +49,18 @@ export default function Home(props: Props) {
 					fechar.
 				</p>
 				<div className={styles.btnSection}>
+					<section className={styles.curve}></section>
 					<div className={styles.title}>
-						<span>{`Já leste os nossos últimos artigos?`}</span>
+						<h3>{`Já leste os nossos últimos artigos?`}</h3>
 					</div>
 					<ImageSlider
 						autoPlay
-						divs={lastArtigos.map(lastArtigo => {
+						divs={lastArtigos.map((lastArtigo, idx) => {
 							return (
-								<div className={styles.outerDiv}>
+								<div
+									key={`${idx}-${lastArtigo.attributes.slug}`}
+									className={styles.outerDiv}
+								>
 									<div className={styles.findMore}>
 										<Artigo artigo={lastArtigo} justHeader />
 										<a
