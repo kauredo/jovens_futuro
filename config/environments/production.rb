e8f441a4b482a6251658fdcd9f32a41e1764c6bb
kauredo <vaskafig@gmail.com>
2021-11-17 17:32:51 +0000

    change development.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  host = 'jovens-futuro.herokuapp.com/'
+  host = 'jovenseofuturo.pt'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: host }
